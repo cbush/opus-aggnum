6940f0f49078bb9107f4867ff878583968cec158
Chris Bush <chris.bush@mongodb.com>
2019-07-18 09:46:04 -0400

    Add toolbox

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -13,6 +13,7 @@ import { PipelineControls } from "./PipelineControls";
 import { Pipeline } from "./Pipeline";
 import { Result } from "./Result";
 import { runPipeline } from "../runPipeline";
+import { Toolbox } from "./Toolbox";
 
 export function Game({ level }) {
   const [stages, setStages] = useState([]);
@@ -28,34 +29,37 @@ export function Game({ level }) {
   const dialog = useDialogState();
   return (
     <>
-      <div className="section">
-        <InputCollections input={input} />
+      <div className="viewport">
+        <div className="section">
+          <InputCollections input={input} />
+        </div>
+        <div className="arrowDown" />
+        <div className="section">
+          <Pipeline input={input} stages={stages} setStages={setStages} />
+          <PipelineControls
+            onAddClicked={() => {
+              setStages([...stages, { id: uuidv1() }]);
+            }}
+          />
+        </div>
+        <div className="section">
+          <Result
+            resultEqualsExpected={resultEqualsExpected}
+            input={result}
+            expected={expectedOutput}
+          />
+          <>
+            <DialogDisclosure disabled={!resultEqualsExpected} {...dialog}>
+              {resultEqualsExpected
+                ? "Result === expected"
+                : "Result does not match expected"}
+            </DialogDisclosure>
+            <DialogBackdrop {...dialog} />
+            <Dialog {...dialog}>Success!</Dialog>
+          </>
+        </div>
       </div>
-      <div className="arrowDown" />
-      <div className="section">
-        <Pipeline input={input} stages={stages} setStages={setStages} />
-        <PipelineControls
-          onAddClicked={() => {
-            setStages([...stages, { id: uuidv1() }]);
-          }}
-        />
-      </div>
-      <div className="section">
-        <Result
-          resultEqualsExpected={resultEqualsExpected}
-          input={result}
-          expected={expectedOutput}
-        />
-        <>
-          <DialogDisclosure disabled={!resultEqualsExpected} {...dialog}>
-            {resultEqualsExpected
-              ? "Result === expected"
-              : "Result does not match expected"}
-          </DialogDisclosure>
-          <DialogBackdrop {...dialog} />
-          <Dialog {...dialog}>Success!</Dialog>
-        </>
-      </div>
+      <Toolbox />
     </>
   );
 }
---
+++ src/components/Toolbox.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "reakit";
+export function Toolbox() {
+  return (
+    <div className="toolbox">
+      <label>TOOLBOX</label>
+      <Button>I am a toolbox</Button>
+    </div>
+  );
+}
